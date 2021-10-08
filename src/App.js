e5a53f18509e64d0c2897629690b005caf2a3e00
Collyngodlewski <collyngodlewski@gmail.com>
2021-10-08 17:38:43 -0400

    i  think i did it!

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+// import { data } from 'msw/lib/types/context';
+import Character from "./components/Character";
+import { BASE_URL } from "./const";
 
 const App = () => {
+  const [names, setNames] = useState([]);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}`)
+      .then((res) => {
+        setNames(res.data);
+        console.log(res.data);
+        return names;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characters={names} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,65 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled, { keyframes } from "styled-components";
+
+
+
+const StyledBox = styled.div`
+display:flex;
+justify-content: space-between;
+margin-left: 10%;
+margin-right: 10%;
+background-color:white;
+opacity: 75%;
+border: 1px solid black;
+
+padding: 10px;
+padding-top: 10px;
+`
+
+
+const StyledName = styled.div`
+padding-left: 5%;
+
+
+
+`
+const StyledYear = styled.div`
+padding-right: 5%;
+`
+
+
+
+
+
+
+
+
+
+
+
+
+const Character = (props) => {
+  const { characters } = props;
+
+  return (
+    <div>
+      {characters.map((res) => {
+        return (
+          <StyledBox>
+            <StyledName>
+                {res.name}
+            </StyledName>
+            <StyledYear>
+                 {res.birth_year}
+            </StyledYear>
+            
+            </StyledBox>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Character;
